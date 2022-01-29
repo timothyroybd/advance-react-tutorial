7ee79695d105f4591faac0ec1d462eb853839315
Timothy <timothyroy94@protonmail.com>
2022-01-29 15:51:55 +0600

    useEfffect Clean Up

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return (
+    <React.Fragment>
+      <button className="btn" onClick={() => setShow(!show)}>
+        Show/Hide
+      </button>
+      {show && <Item />}
+    </React.Fragment>
+  );
+};
+
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  }, []);
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      <h1>Window</h1>
+      <h2>Size : {size} </h2>
+    </div>
+  );
 };
 
 export default ShowHide;
