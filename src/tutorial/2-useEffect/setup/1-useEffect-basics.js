449c7ef80ecca1665fa02ed5214b12dd6e8cc31f
Timothy <timothyroy94@protonmail.com>
2022-01-29 10:57:02 +0600

    useEffect Basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log("logging from useEffect");
+    if (value > 0) {
+      document.title = `New Messages (${value})`;
+    }
+  }, [value]);
+  console.log("render component in the parent outside of useEffect");
+  return (
+    <React.Fragment>
+      <h2>{value}</h2>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Click Me
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectBasics;
