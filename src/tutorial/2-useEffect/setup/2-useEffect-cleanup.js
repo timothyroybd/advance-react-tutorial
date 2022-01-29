c5076fbfd05741b6b698a1651ad8ef14c117f5da
Timothy <timothyroy94@protonmail.com>
2022-01-29 13:21:57 +0600

    useeffect using git API

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      console.log("cleanup");
+      window.removeEventListener("resize", checkSize);
+    };
+  });
+  // console.log(size);
+  return (
+    <React.Fragment>
+      <h1>Window</h1>
+      <h2>{size}</h2>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectCleanup;
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    setUsers(users);
+    // console.log(users);
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+  return (
+    <>
+      <h3>Github Users</h3>
+      <section className="container users">
+        {users.map(({ login, id, avatar_url, url, html_url }) => {
+          return (
+            <article key={id}>
+              <img src={avatar_url} alt="" />
+              <h3>{login}</h3>
+              <a href={html_url} target="_blank">
+                Github
+              </a>
+            </article>
+          );
+        })}
+      </section>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
